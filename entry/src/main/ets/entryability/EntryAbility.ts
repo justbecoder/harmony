ec5b89e7c36285799fc8cc6286645bda48d3b9e6
justbecoder <justbecoder@aliyun.com>
2023-11-21 17:25:43 +0800

    feat: harmony_fruit

---
+++ entry/src/main/ets/entryability/EntryAbility.ts
@@ -0,0 +1,71 @@
+import UIAbility from '@ohos.app.ability.UIAbility';
+import hilog from '@ohos.hilog';
+import window from '@ohos.window';
+import router from '@ohos.router';
+import featureAbility from '@ohos.ability.featureAbility';
+import wantConstant from '@ohos.ability.wantConstant';
+
+export default class EntryAbility extends UIAbility {
+  onCreate(want, launchParam) {
+    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onCreate');
+  }
+
+  onDestroy() {
+    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onDestroy');
+  }
+
+  onWindowStageCreate(windowStage: window.WindowStage) {
+    // Main window is created, set main page for this ability
+    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageCreate');
+
+    windowStage.loadContent('pages/Index', (err, data) => {
+      if (err.code) {
+        hilog.error(0x0000, 'testTag', 'Failed to load the content. Cause: %{public}s', JSON.stringify(err) ?? '');
+        return;
+      }
+      hilog.info(0x0000, 'testTag', 'Succeeded in loading the content. Data: %{public}s', JSON.stringify(data) ?? '');
+    });
+  }
+
+  onWindowStageDestroy() {
+    // Main window is destroyed, release UI related resources
+    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageDestroy');
+  }
+
+  onForeground() {
+    // Ability has brought to foreground
+    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onForeground');
+  }
+
+  onBackground() {
+    // Ability has back to background
+    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onBackground');
+  }
+
+  jumpToTodo () {
+    console.log("this is a test")
+
+    // router.pushUrl({
+    //   url: 'pages/Profile',
+    //   params: {
+    //     name: "胡小帅"
+    //   }
+    // })
+    // this.context.startAbility({
+    //   abilityName: "Todo",
+    // })
+    // featureAbility.startAbility({
+    //   want: {
+    //     abilityName: "Todo"
+    //   }
+    // })
+
+    // 跳转系统设置页面
+    // featureAbility.startAbility({
+    //   want: {
+    //     moduleName: "Todo",
+    //     abilityName: "Todo"
+    //   }
+    // })
+  }
+}
